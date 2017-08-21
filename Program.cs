18cfe70c32b9e826e54c0f3a5cd9d3092c3373ec
Larry Johnston <ljohnston@sepialine.com>
2017-08-21 11:57:16 -0700

    Project using package reference style.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+using Lextm.SharpSnmpLib;
+
+namespace DependencyIssue {
+    public class Program {
+        static void Main() {
+            Console.Out.WriteLine(typeof(ObjectIdentifier).GetCustomAttributes(typeof(Attribute)));
+            Console.ReadLine();
+        }
+    }
+}
